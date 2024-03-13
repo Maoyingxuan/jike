04a6273981d902181133e196d074b93feff6f9ed
黑岸 <oncwnuHM1_UwSYXbeIPs6OYsHrYI@git.weixin.qq.com>
2024-03-13 20:46:54 +0800

    `登录页-静态

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,31 @@
-const Login = ()=>{
-    return(
-        <div>this is Login</div>
+import { Card, Form, Input, Button, Checkbox } from 'antd'
+import './index.scss'
+const Login = () => {
+    return (
+      <div className="login">
+        <Card className="login-container">
+        <div className="title">登录</div>
+          {/* 登录表单 */}
+          <Form>
+      <Form.Item>
+        <Input size="large" placeholder="请输入手机号" />
+      </Form.Item>
+      <Form.Item>
+        <Input size="large" placeholder="请输入验证码" />
+      </Form.Item>
+      <Form.Item>
+        <Checkbox className="login-checkbox-label">
+          我已阅读并同意「用户协议」和「隐私条款」
+        </Checkbox>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" size="large" block>
+          登录
+        </Button>
+      </Form.Item>
+    </Form>
+        </Card>
+      </div>
     )
-}
+  }
 export default Login
