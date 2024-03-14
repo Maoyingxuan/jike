67f630ec4db0bea9203f2a4385c6a0283f1e14c3
黑岸 <oncwnuHM1_UwSYXbeIPs6OYsHrYI@git.weixin.qq.com>
2024-03-14 13:12:15 +0800

    token失效处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 // axios封装
+import router from '@/router'
 import axios from 'axios'
-import { getToken } from './token'
+import { getToken, removeToken } from './token'
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
   timeout: 5000
@@ -23,6 +24,13 @@ request.interceptors.response.use((response)=> {
   }, (error)=> {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    // 监控401
+    console.dir(error)
+    if(error.response.status === 401){
+      removeToken()
+      router.navigate('/login')
+      window.location.reload()
+    }
     return Promise.reject(error)
 })
 
